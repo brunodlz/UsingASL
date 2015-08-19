aa0c7e4e43b095733a6a38b3193d115aeed1f15a
Bruno da Luz <bruno@belab.com.br>
2015-08-19 09:55:22 -0300

    first commit

---
+++ UsingASL/LogAsl.h
@@ -0,0 +1,13 @@
+//
+//  LogAsl.h
+//  UsingASL
+//
+//  Created by Bruno da Luz on 8/19/15.
+//  Copyright (c) 2015 bruno v0id. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LogAsl : NSObject
+
+@end
---
+++ UsingASL/LogAsl.m
@@ -0,0 +1,13 @@
+//
+//  LogAsl.m
+//  UsingASL
+//
+//  Created by Bruno da Luz on 8/19/15.
+//  Copyright (c) 2015 bruno v0id. All rights reserved.
+//
+
+#import "LogAsl.h"
+
+@implementation LogAsl
+
+@end
